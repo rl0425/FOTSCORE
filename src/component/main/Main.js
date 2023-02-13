78f87cb2147e9d7a3937e8abf5c03da9dc4b9d87
parkgichan <rl0425@naver.com>
2023-02-13 20:28:17 +0900

    commit

---
+++ src/component/main/Main.js
@@ -0,0 +1,22 @@
+// import Fotmob from 'fotmob';
+// const fotmob = new Fotmob();
+
+function Main(){
+    // async function getData(){
+    //     let matches = await fotmob.getMatchesByDate("20201020");
+    //     let league = await fotmob.getLeague("42", "overview", "league", "America/New_York")
+    //     let team = await fotmob.getTeam("6017", "overview", "team", "America/New_York")
+    //     let player = await fotmob.getPlayer("1071179")
+    //     let details = await fotmob.getMatchDetails("3399269")
+    //
+    //     console.log("matches = ", matches)
+    // }
+    //
+    // getData()
+
+    return (
+        <div>si</div>
+    )
+}
+
+export default Main
