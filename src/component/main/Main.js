736078b692f1f2a8bd196de6bf2205af8ba400ca
박기찬 <rl0425@naver.com>
2023-02-15 22:30:24 +0900

    commit

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -1,18 +1,14 @@
-// import Fotmob from 'fotmob';
-// const fotmob = new Fotmob();
+import {useEffect} from "react";
 
 function Main(){
-    // async function getData(){
-    //     let matches = await fotmob.getMatchesByDate("20201020");
-    //     let league = await fotmob.getLeague("42", "overview", "league", "America/New_York")
-    //     let team = await fotmob.getTeam("6017", "overview", "team", "America/New_York")
-    //     let player = await fotmob.getPlayer("1071179")
-    //     let details = await fotmob.getMatchDetails("3399269")
-    //
-    //     console.log("matches = ", matches)
-    // }
-    //
-    // getData()
+    const getData = async () => {
+        const res = await fetch('https://site.api.espn.com/apis/site/v2/sports/soccer/eng.1/scoreboard?&dates=20230210-20230321').then((res) => res.json());
+        // const res = await fetch('https://apiv2.allsportsapi.com/football/?met=Leagues&APIkey=f4221bf5a0090379a1f7fca4e225ade743e06434a9ff5839e870c9ee20fb2e73').then((res) => res.json());
+    }
+
+    useEffect(() => {
+        getData(); //API 호출
+    }, []);
 
     return (
         <div>si</div>
